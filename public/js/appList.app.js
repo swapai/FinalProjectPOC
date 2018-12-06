f54f26cbc61d10d8650a347bad59086d5bd7cb97
Swati Pai <swapai@atlassian.com>
2018-12-05 21:10:35 -0500

    App page changes

--- public/js/appList.app.js
+++ public/js/appList.app.js
@@ -1,36 +1,38 @@
 var listOfApps = new Vue({
   el: '#appsMain',
   data: {
+    userId:"",
+    appNumberNotifiction:0,
     appList:[
       {
         appNum:1,
-        appName: "Name of first App",
-        appDescription: "Description of the App"
+        appName: "Sales OTrack",
+        appDescription: "The salesman's companion to identify checkpoints and determine order flow"
       },
       {
         appNum:2,
-        appName: "Name of second App",
-        appDescription: "Description of the App"
+        appName: "Smart Diagnostics",
+        appDescription: "Make smarter decisions to improve engine operational efficiency "
       },
       {
         appNum:3,
-        appName: "Name of third App",
-        appDescription: "Description of the App"
+        appName: "JIRA",
+        appDescription: "Plan, track, and manage agile and software development projects"
       },
       {
         appNum:4,
-        appName: "Name of fourth App",
-        appDescription: "Description of the App"
+        appName: "Splunk",
+        appDescription: "Real-time visibility to improve application delivery"
       },
       {
         appNum:5,
-        appName: "Name of fifth App",
-        appDescription: "Description of the App"
+        appName: "AWS CodeDeploy",
+        appDescription: "The fully managed deployment service that automates software deployments"
       },
       {
         appNum:6,
-        appName: "Name of sixth App",
-        appDescription: "Description of the App"
+        appName: "Selenium",
+        appDescription: "The portable software-testing framework for web applications"
       }
     ]
 },
@@ -38,16 +40,25 @@ var listOfApps = new Vue({
 
 methods: {
   gotoWebsiteNext() {
-      window.location = 'clientList.html';
+      window.location = 'clientList.html'+'?appNumber='+this.appNumberNotifiction;
   }
 },
 
   created (){
 
-    // const url = new URL(window.location.href);
-    // const clientId = url.searchParams.get('clientId');
-    // console.log('Client Id: '+ clientId);
-    // this.site.clientId = clientId;
+    const url = new URL(window.location.href);
+    const userId = url.searchParams.get('user');
+    console.log('User Id: '+ userId);
+    this.userId = userId;
+    if(this.userId == "sales" || this.userId == "service"){
+      if(this.userId == "sales"){
+        this.appNumberNotifiction = 1;
+      }else{
+        this.appNumberNotifiction = 2;
+      }
+    }else{
+      this.appNumberNotifiction = 0;
+    }
     //
     // fetch('api/site.php?clientId='+clientId)
     // .then( response => response.json() )
