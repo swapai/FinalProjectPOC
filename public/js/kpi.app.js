4b79fd3d993a0a6277710cc711deaffbf35d10fb
Swati Pai <swapai@atlassian.com>
2018-12-08 22:23:22 -0500

    wasdsa

--- public/js/kpi.app.js
+++ public/js/kpi.app.js
@@ -242,58 +242,59 @@ var kpiApp = new Vue({
                 data: this.kpiList.map( item => [Date.parse(item.dataCollectiveDate), Number(item.trips)] )
             }]
         });
-  },buildAvailabilityAndReliabilityChart(){
-      Highcharts.chart('availabilityAndReliabilityChart', {
-        chart: {
-      type: 'areaspline'
   },
-  title: {
-      text: 'Availability and reliability'
-  },
-  legend: {
-      layout: 'vertical',
-      align: 'left',
-      verticalAlign: 'top',
-      x: 150,
-      y: 100,
-      floating: true,
-      borderWidth: 1,
-      backgroundColor: (Highcharts.theme && Highcharts.theme.legendBackgroundColor) || '#FFFFFF'
-  },
-  xAxis: {
-      type: 'datetime',
-      plotBands: [{
-          from: 4.5,
-          to: 6.5,
-          color: 'rgba(68, 170, 213, .2)'
-      }]
-  },
-  yAxis: {
-      title: {
-          text: 'Availability and Reliability'
-      }
-  },
-  tooltip: {
-      shared: true,
-      valueSuffix: ' units'
-  },
-  credits: {
-      enabled: false
-  },
-  plotOptions: {
-      areaspline: {
-          fillOpacity: 0.5
-      }
-  },
-  series: [{
-      name: 'Availability',
-      data: this.kpiList.map( item => [Date.parse(item.dataCollectiveDate), Number(item.availability)] )
-  }, {
-      name: 'Reliability',
-      data: this.kpiList.map( item => [Date.parse(item.dataCollectiveDate), Number(item.reliability)] )
-    }]
-  });
-  },
+  //buildAvailabilityAndReliabilityChart(){
+  //     Highcharts.chart('availabilityAndReliabilityChart', {
+  //       chart: {
+  //     type: 'areaspline'
+  // },
+  // title: {
+  //     text: 'Availability and reliability'
+  // },
+  // legend: {
+  //     layout: 'vertical',
+  //     align: 'left',
+  //     verticalAlign: 'top',
+  //     x: 150,
+  //     y: 100,
+  //     floating: true,
+  //     borderWidth: 1,
+  //     backgroundColor: (Highcharts.theme && Highcharts.theme.legendBackgroundColor) || '#FFFFFF'
+  // },
+  // xAxis: {
+  //     type: 'datetime',
+  //     plotBands: [{
+  //         from: 4.5,
+  //         to: 6.5,
+  //         color: 'rgba(68, 170, 213, .2)'
+  //     }]
+  // },
+  // yAxis: {
+  //     title: {
+  //         text: 'Availability and Reliability'
+  //     }
+  // },
+  // tooltip: {
+  //     shared: true,
+  //     valueSuffix: ' units'
+  // },
+  // credits: {
+  //     enabled: false
+  // },
+  // plotOptions: {
+  //     areaspline: {
+  //         fillOpacity: 0.5
+  //     }
+  // },
+  // series: [{
+  //     name: 'Availability',
+  //     data: this.kpiList.map( item => [Date.parse(item.dataCollectiveDate), Number(item.availability)] )
+  // }, {
+  //     name: 'Reliability',
+  //     data: this.kpiList.map( item => [Date.parse(item.dataCollectiveDate), Number(item.reliability)] )
+  //   }]
+  // });
+  // },
   buildHeatRateChart(){
     Highcharts.chart('heatRateChart', {
     chart: {
@@ -332,62 +333,62 @@ var kpiApp = new Vue({
     }]
 })
 },
-buildTripsAndStartsChart(){
-  Highcharts.chart('tripsAndStartsChart', {
-
-    title: {
-        text: 'Trips and Starts Plot'
-    },
-    yAxis: {
-        title: {
-            text: 'Number of Trips and Starts'
-        }
-    },
-    xAxis: {
-        type: 'datetime',
-    },
-    legend: {
-        layout: 'vertical',
-        align: 'right',
-        verticalAlign: 'middle'
-    },
-    plotOptions: {
-        series: {
-            label: {
-                connectorAllowed: false
-            },
-            color: 'purple',
-            enableMouseTracking: true,
-            pointStart: 2010
-        }
-    },
-    series: [{
-      type:'line',
-        name: 'Trips',
-        data:  this.kpiList.map( item => [Date.parse(item.dataCollectiveDate), Number(item.trips)] )
-    }, {
-        type:'line',
-        name: 'Starts',
-        data: this.kpiList.map( item => [Date.parse(item.dataCollectiveDate), Number(item.starts)] )
-    }],
-
-    responsive: {
-        rules: [{
-            condition: {
-                maxWidth: 500
-            },
-            chartOptions: {
-                legend: {
-                    layout: 'horizontal',
-                    align: 'center',
-                    verticalAlign: 'bottom'
-                }
-            }
-        }]
-    }
-
-})
-},
+// buildTripsAndStartsChart(){
+//   Highcharts.chart('tripsAndStartsChart', {
+//
+//     title: {
+//         text: 'Trips and Starts Plot'
+//     },
+//     yAxis: {
+//         title: {
+//             text: 'Number of Trips and Starts'
+//         }
+//     },
+//     xAxis: {
+//         type: 'datetime',
+//     },
+//     legend: {
+//         layout: 'vertical',
+//         align: 'right',
+//         verticalAlign: 'middle'
+//     },
+//     plotOptions: {
+//         series: {
+//             label: {
+//                 connectorAllowed: false
+//             },
+//             color: 'purple',
+//             enableMouseTracking: true,
+//             pointStart: 2010
+//         }
+//     },
+//     series: [{
+//       type:'line',
+//         name: 'Trips',
+//         data:  this.kpiList.map( item => [Date.parse(item.dataCollectiveDate), Number(item.trips)] )
+//     }, {
+//         type:'line',
+//         name: 'Starts',
+//         data: this.kpiList.map( item => [Date.parse(item.dataCollectiveDate), Number(item.starts)] )
+//     }],
+//
+//     responsive: {
+//         rules: [{
+//             condition: {
+//                 maxWidth: 500
+//             },
+//             chartOptions: {
+//                 legend: {
+//                     layout: 'horizontal',
+//                     align: 'center',
+//                     verticalAlign: 'bottom'
+//                 }
+//             }
+//         }]
+//     }
+//
+// })
+// },
 buildCompressorEfficiencyChart(){
   Highcharts.chart('compressorEfficiencyChart', {
     title: {
