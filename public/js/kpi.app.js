eebb8986bc674e9151e98a195f9af097bf231f90
Swati Pai <swapai@atlassian.com>
2018-12-08 21:42:01 -0500

    ewrads

--- public/js/kpi.app.js
+++ public/js/kpi.app.js
@@ -271,6 +271,17 @@ buildCompressorEfficiencyChart(){
                 lineWidth: 1,
             }
         },
+        tooltip: {
+            backgroundColor: {
+                linearGradient: [0, 0, 0, 60],
+                stops: [
+                    [0, '#CCCCCC'],
+                    [1, '#E0E0E0']
+                ]
+            },
+            borderWidth: 1,
+            borderColor: '#331a1c'
+        },
 
     series: [{
         name: 'Operational Efficiency (%)',
